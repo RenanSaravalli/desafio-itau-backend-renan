b52c172d0fd09081265e7a14b1432781be01737a
RenanSaravalli <renansaravalli@gmail.com>
2026-05-16 11:47:45 -0300

    Ajusta aplicação para buscar classe de configuração

--- src/main/java/renan/saravalli/desafioItau/DesafioItauApplication.java
+++ src/main/java/renan/saravalli/desafioItau/DesafioItauApplication.java
@@ -2,8 +2,10 @@ package renan.saravalli.desafioItau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DesafioItauApplication {
 
 	public static void main(String[] args) {
