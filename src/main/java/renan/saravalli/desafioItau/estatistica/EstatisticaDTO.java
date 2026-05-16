4130236583627416a929ded55bdb830b1a32a16b
RenanSaravalli <renansaravalli@gmail.com>
2026-05-16 11:49:41 -0300

    Adiciona `EstatisticaDTO.java`

---
+++ src/main/java/renan/saravalli/desafioItau/estatistica/EstatisticaDTO.java
@@ -0,0 +1,20 @@
+package renan.saravalli.desafioItau.estatistica;
+
+import lombok.Getter;
+
+@Getter
+public class EstatisticaDTO {
+    private final long count;
+    private final Double sum;
+    private final Double avg;
+    private final Double min;
+    private final Double max;
+
+    public EstatisticaDTO(long count, Double sum, Double avg, Double min, Double max) {
+        this.count = count;
+        this.sum = sum;
+        this.avg = avg;
+        this.min = min;
+        this.max = max;
+    }
+}
